80327d73da92829cb3b4b4c7269844e1d17bfb62
gabrielcarlossl <gabrielcarlossleite@gmail.com>
2022-10-23 20:33:17 -0300

    exemplo de concat

---
+++ concat_01.js
@@ -0,0 +1,7 @@
+const nomesMasc = ['alex', 'josé', 'hugo', 'caio']
+const nomesFem = [ 'maria', 'socorro', 'lely']
+
+const todos = nomesMasc.concat(nomesFem)
+console.log(todos)
+
+console.log(['a','b'].concat([1,2],[3,4],5, [[6,7]]))
